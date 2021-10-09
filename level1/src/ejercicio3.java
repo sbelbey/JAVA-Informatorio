11546f9f1515788b5960b4acc6484bf654d7645b
Saúl Belbey <88008362+sbelbey@users.noreply.github.com>
2021-10-09 11:35:31 -0300

    ejercicio3

--- level1/src/ejercicio3.java
+++ level1/src/ejercicio3.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
+//Confeccionar un programa que dado un número entero como dato de entrada,
+// imprima la secuencia de números (incrementos de 1) de la siguiente forma:
+//1
+//1 2
+//1 2 3
+//1 2 3 4
+//1 2 3 4 5
 public class ejercicio3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Integer numero = Integer.parseInt(scan.nextLine());
+
+        for (int i = 1; i <= numero; i++){
+            for (int j = 1; j<=i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
 }
