ec2f7d767413478e8a4d49363c4196bc63a08b8f
Saúl Belbey <88008362+sbelbey@users.noreply.github.com>
2021-10-10 19:03:33 -0300

    ejercicio6

---
+++ level1/src/ejercicio6.java
@@ -0,0 +1,23 @@
+//Se desea una aplicación que solicite 2 números enteros y realice la operación de potencia (sin uso de librerías).
+
+import java.util.Scanner;
+
+public class ejercicio6 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese el numero base: ");
+        Integer num1 = Integer.valueOf(scan.nextLine());
+        System.out.println("Ingrese el exponente: ");
+        Integer num2 = Integer.valueOf(scan.nextLine());
+
+        System.out.println(num1 + " elevado a " + num2 + " = " + elevar(num1, num2));
+    }
+
+    public static Integer elevar (Integer base, Integer exponente){
+        Integer acumulador = 1;
+        for (int i = 1; i<=(exponente); i++){
+            acumulador = acumulador*base;
+        }
+        return acumulador;
+    }
+}
