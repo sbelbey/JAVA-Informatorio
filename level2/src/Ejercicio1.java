e8fc7efb327c047d635d5e1af46a95897a2e00b9
Saúl Belbey <88008362+sbelbey@users.noreply.github.com>
2021-10-10 21:56:09 -0300

    ejercicio1

---
+++ level2/src/Ejercicio1.java
@@ -0,0 +1,37 @@
+//Crear un ArrayList y cargarlo con tus ciudades favoritas de Argentina,
+// luego imprimir por pantalla el ranking
+//Input (Entrada):
+//Bariloche
+//Córdoba
+//Resistencia
+//
+//Output (Salida):
+//#1 - Bariloche
+//#2 - Córdoba
+//#3 - Resistencia
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Integer control = 0;
+        ArrayList<String> ciudades = new ArrayList<>();
+
+        do {
+            System.out.println("Ingresá de a una tus ciudades argentinas favoritas:");
+            ciudades.add(String.valueOf(scan.nextLine()));
+            System.out.println("Para salir ingresá 1, para continuar seleccioná cualquier otro número.");
+            control = Integer.valueOf(scan.nextLine());
+
+        }while (control != 1);
+        scan.close();
+
+        System.out.println("La el ranking de ciudades es: ");
+        for (int i = 0; i< ciudades.size(); i++){
+            System.out.println("#" + (i+1) + " - " + ciudades.get(i));
+        }
+    }
+}
